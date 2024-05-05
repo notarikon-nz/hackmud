f7962cbd9a1b38c604bb8461278311aa9861d177
Matt Orsborn <57729211+notarikon-nz@users.noreply.github.com>
2024-05-05 17:15:43 +1200

    Update bank.js

--- utility/bank.js
+++ utility/bank.js
@@ -1,1 +1,28 @@
+function (context, args, username){
+
+	username = context.caller; // get username of person calling script
+
+	if (!args) return; // if no args, silently return
+
+	if (!authorise())  // if not an authorised user, return
+		return 	"`DError 403``1 : Forbidden`\n`1You don't have permission to access this server`"
+
+	if (args.withdraw) {
+		return withdraw(args.withdraw)
+	}
+	
+	return;
+
+  // 
+	function authorise() {
+    // alternatively, use caller.calling_script and implement security on that end
+		return (username == "your_username_here" || username == "your_alts_username_here") // this should be a database implementation
+	}
+
+	function withdraw(amount) {
+		if (isNaN(amount)) return;
+		return JSON.stringify( #ms.accts.xfer_gc_to({to:username,amount:amount}) )
+	}
+
+}
 
