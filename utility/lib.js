ab8c0f7d30c5ecf9871038a1c39406bd4a5c2220
Matt Orsborn <57729211+notarikon-nz@users.noreply.github.com>
2024-05-05 17:27:53 +1200

    Update lib.js

--- utility/lib.js
+++ utility/lib.js
@@ -1,13 +1,10 @@
-function(context, args, username){
+function(context, args){
 
-	username = context.caller;
-
+	// add authorisation here
+	
 	if(!args) {
 		return JSON.stringify(context);
 	}
-
-	if (!authorise())
-		return 	"`DError 403``1 : Forbidden`\n`1You don't have permission to access this server`"
 
 	if (!args) return;
 
@@ -22,7 +19,4 @@ function(context, args, username){
 	
 	return { ok:false };
 
-	function authorise(username) {
-		return (username == "notarikon") // this should be a database implementation
-	}
 }
