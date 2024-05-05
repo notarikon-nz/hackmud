48505af89529e439fc183f45f441f65190224520
Matt Orsborn <57729211+notarikon-nz@users.noreply.github.com>
2024-05-05 17:21:53 +1200

    Update lib.js

--- utility/lib.js
+++ utility/lib.js
@@ -1,1 +1,28 @@
+function(context, args, username){
 
+	username = context.caller;
+
+	if(!args) {
+		return JSON.stringify(context);
+	}
+
+	if (!authorise())
+		return 	"`DError 403``1 : Forbidden`\n`1You don't have permission to access this server`"
+
+	if (!args) return;
+
+	switch(args.get) {
+
+		case 'l0c': return "".split(',') // l0ck3t & l0ckbox keys
+		case 'prj' : return "".split(",") // known project names
+		case 'usr' : return "".split(",") // known usernames
+		case 'phr' : return "".split(",") // known passphrases
+
+	}
+	
+	return { ok:false };
+
+	function authorise(username) {
+		return (username == "notarikon") // this should be a database implementation
+	}
+}
