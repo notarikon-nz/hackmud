792f671faaaa93c5dd62cef3ee3328eeaf2e9dc2
Matt Orsborn <57729211+notarikon-nz@users.noreply.github.com>
2024-05-05 18:23:14 +1200

    Update lib.js

--- utility/lib.js
+++ utility/lib.js
@@ -16,6 +16,7 @@ function(context, args){
 		case 'prj' : return "".split(",") // known project names
 		case 'usr' : return "".split(",") // known usernames
 		case 'phr' : return "".split(",") // known passphrases
+		case 'pub' : return "amal_robo,aon,archaic,bluebun,bunnybat_hut,context,core,cyberdine,empty_nest,etceteraco,futuretech,goodfellow,halperyon,kill_9_1,kill_bio,legion_bible,legion_intl,light,lowell_extermination,marco_polo,merrymoor_pharma,nation_of_wales,nogrub,nuutec,pica,protein_prevention,ros_13_update_checker,ros13,setec_gas,skimmerite,sn_w,soylentbean,subject_object,suborbital_airlines,tandoori,the_holy_checksum,turing_testing,tyrell,vacuum_rescue,weathernet,welsh_measles_info,weyland,world_pop".split(",")
 
 	}
 	
