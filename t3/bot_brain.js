a6413f134dbff0ea9f3c53de507d4fd01ebd5133
Matt Orsborn <57729211+notarikon-nz@users.noreply.github.com>
2024-05-06 23:32:36 +1200

    Update bot_brain.js

--- t3/bot_brain.js
+++ t3/bot_brain.js
@@ -1,1 +1,26 @@
+function(a,g) {
+
+/*
+{
+  rarity: "h4x0r",
+  name: "cron_bot_v2",
+  type: "bot_brain",
+  up_class: "architect",
+  tier: 2,
+  loaded: false,
+  cooldown: 1166,
+  cost: 49718,
+  retries: 2,
+  last_time: 1714983105446,
+  sn: "663864ce1be6dd2abd5bd60b",
+  description: "bot_brain upgrades will call your bot_brain script based on the upgrade's cooldown interval, allowing you to automate your user.",
+  i: 70
+}
+*/
+
+
+
+}
+
+
 
