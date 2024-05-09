76fefc241770356c85c8617307cd5f0cc1416a94
Matt Orsborn <57729211+notarikon-nz@users.noreply.github.com>
2024-05-09 12:15:04 +1200

    Update clairvoyance.js

--- t3/clairvoyance.js
+++ t3/clairvoyance.js
@@ -1,1 +1,82 @@
 
+function(context, args) //t:#s.context.internal
+{
+    var caller = context.caller
+    var l = #fs.scripts.lib(),
+        response, 
+		    username, 
+		    call_count = 0,
+		    target_name = "",
+		    current_date,
+		    state_info
+
+    if (!args || !args.t) {
+        return "Provide a target with t:#s.corp.private, then repeatedly run until prompted to enter the provided backup server as a scriptor.\nNow run it again with the scriptor until process is complete."
+    }
+
+	let usernames = #fs.notarikon.lib( {get:"usernames"} )
+
+	initialise_state(context, args);
+
+    while (!check_timeout() && call_count < 222) {
+        switch (state_info.run_time_state) {
+            case 0:
+                handle_find_username();
+                break;
+            case 1:
+                handle_find_pin();
+                break;
+            case 2:
+                gather_calendar_ids();
+                break;
+            case 3:
+                collect_info_from_calendar();
+                break;
+            case 4:
+                find_backup_server();
+                break;
+            case 5:
+                output_backup_server_name();
+                break;
+            case 6:
+                scrape_backup_server_for_key();
+                break;
+            case 7:
+                decrypt_calendar_info();
+                break;
+            case 8:
+                final_processing();
+                break;
+            default:
+                throw new Error("Invalid state");
+        }
+    }
+
+    return finalise_state();
+
+  // ---------------------------------------
+	function initialise_state(context, args) {
+
+    state_info = #db.f({script:context.this_script, caller}).first()
+		current_date = Date.now()
+
+		if (!state_info || current_date - state_info.lastRunDate > 144e5 || args.reset === true)
+		{
+				state_info = {
+					script: context.this_script,
+					caller,
+					last_run_date: current_date,
+					run_time_state: 0,
+					username: 0,
+					pin: 0,
+					calendar_ids: [],
+					enumeration: -8,
+					enumeration2: 0,
+					id_content: [],
+					data_object: {}
+				}
+	  }
+	  target_name = args.t.name.split(".")[0]
+
+	}  
+}
