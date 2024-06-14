7f7c1f9992ac4e546d4b935c0e3eaff0b17b23e2
Matt Orsborn <57729211+notarikon-nz@users.noreply.github.com>
2024-06-14 15:14:18 +1200

    Update bot_brain.js

--- t3/bot_brain.js
+++ t3/bot_brain.js
@@ -1,26 +1,47 @@
-function(a,g) {
+function (c,a) {
 
-/*
-{
-  rarity: "h4x0r",
-  name: "cron_bot_v2",
-  type: "bot_brain",
-  up_class: "architect",
-  tier: 2,
-  loaded: false,
-  cooldown: 1166,
-  cost: 49718,
-  retries: 2,
-  last_time: 1714983105446,
-  sn: "663864ce1be6dd2abd5bd60b",
-  description: "bot_brain upgrades will call your bot_brain script based on the upgrade's cooldown interval, allowing you to automate your user.",
-  i: 70
+  // automatically checks if T3 corp domains are active, adds anything new, and deletes expired domains
+  // Ideally run on a ~10 minute timer, at a cost of 5-6MGC per day
+  
+    const prefix = "archaic,context,core,futuretech,halperyon,light,nuutec,sn_w".split(","),
+    suffix = "private,internal,priv,employees,intern,employee_login,emplogin".split(","),
+    t3 = new Array();
+
+    const group = "activeDomains";
+
+    const existingEntries = #db.f({group}).array();
+
+    prefix.forEach(first => {
+        suffix.forEach(second => {
+            
+            const domain = first + "." + second 
+            const existing = existingEntries.find(f => f.domain == domain);
+
+            const r = #fs.scripts.get_level({ name: domain })
+
+            if (Number.isInteger(r) && (r == 1 || r == 0)) {
+                  t3.push(domain)
+                  if (!existing) domainToDatabase(domain);
+            } else {
+
+                if (existing) removeDomainFromDatabase(domain);
+            }
+        })
+    });
+	
+    function domainToDatabase(domain) {
+        const timestamp = _START;
+        const query = { group, domain };
+        const update = { $set: { timestamp } };
+        #db.us(
+            query,
+            update //, { upsert: true }
+        );    
+    }
+
+    function removeDomainFromDatabase(domain) {
+        const query = { group, domain };
+        #db.r(query);
+    }
+
 }
-*/
-
-
-
-}
-
-
-
